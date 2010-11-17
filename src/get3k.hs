30bdc28887465eca910f28346a2cb39ebdd9e90b
Daniel Corson <danl@alum.mit.edu>
2010-11-17 11:52:01 -1000

    get3k work; make1 str->int comp err

--- src/get3k.hs
+++ src/get3k.hs
@@ -1,12 +1,12 @@
 import Control.Applicative
 import Data.Char
+import Data.List
 import Data.List.Split
 import Data.Maybe
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Match
 import qualified Data.ByteString as BS
-
-s = BS.pack . map (fromIntegral . ord)
+import qualified Codec.Binary.UTF8.String as U8
 
 tagTextMaybe (TagText s) = Just s
 tagTextMaybe _ = Nothing
@@ -15,10 +15,17 @@ onTail f [] = f []
 onTail f (a:b) = a : f b
 
 f = do
-  tags <- parseTags <$> BS.readFile "3000char.html"
+  tags <- parseTags <$> readFile "3k.html"
   let
-    rows = take 2400 . drop 4 $ splitWhen (== TagOpen (s "tr") []) tags
-  return $ map (onTail (onTail ((:[]) . BS.concat)) . catMaybes . 
-    map tagTextMaybe) rows
+    rows = take 2400 . drop 4 $ splitWhen (== TagOpen "tr" []) tags
+  return $ map ((\ (n:z:d) -> 
+      z ++ "\t" ++ n ++ " " ++ concat d) . 
+    catMaybes . map tagTextMaybe) rows
 
-main = f >>= print
+main = do
+  r <- f
+  putStr $ unlines $ map (concatMap esc . init) r
+
+esc '\n' = "\\n"
+esc '\\' = "\\\\"
+esc c = [c]
--- src/make1.hs
+++ src/make1.hs
@@ -1,11 +1,17 @@
 import Data.List
 import Control.Applicative
 import qualified Data.Map as M
+import System.Environment
+import System.FilePath
 
 listToPair [a,b] = (a,b)
 
 highSndOrBKey (a,n) (b,m) = if n > m then (a,n) else (b,m)
 
 main = do
-  a <- unlines . map (\ (a,b) -> a ++ " " ++ b) . M.toList . M.map fst . M.fromListWith highSndOrBKey . map ((\ [a,b,c] -> (a,(b,c))) . words) . lines <$> readFile "wubi.txt"
-  writeFile "wubi1.txt" a
+  home <- getEnv "HOME"
+  a <- unlines . map (\ (a,b) -> a ++ " " ++ b) . M.toList . M.map fst . 
+    M.fromListWith highSndOrBKey . 
+    map ((\ [a,b,c] -> (a,(b,read c :: Int))) . words) . 
+    lines <$> readFile (home </> "l" </> "l" </> "z" </> "wubi.txt")
+  writeFile (home </> "l" </> "l" </> "z" </> "wubi1.txt") a
