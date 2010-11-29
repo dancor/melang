38a4750a01ba12c74d17926eadef77276ac87c24
Daniel Corson <danl@alum.mit.edu>
2010-11-29 12:06:28 -1000

    unicode + backspace

--- src/WLk.hs
+++ src/WLk.hs
@@ -5,27 +5,29 @@ import Data.Ord
 import Control.Applicative
 import Control.Arrow
 import Control.Monad
+import Control.Monad.Trans
+import System.Console.Haskeline
 import System.Environment
 import System.FilePath
-import System.IO
 import qualified Data.Map as M
 
 import PairUtil
 import Wubi
 
-main = do
-  home <- getEnv "HOME"
-  wubiToCh <- map (listToPair . take 2 . words) . lines <$> 
+io = liftIO
+
+main = runInputT defaultSettings $ do
+  home <- io $ getEnv "HOME"
+  wubiToCh <- io $ map (listToPair . take 2 . words) . lines <$> 
     readFile (home </> "l" </> "l" </> "z" </> "wubi1.txt")
   let
     w2c = M.fromList wubiToCh
     c2w = M.fromListWith (++) $ map (second (:[]) . swap) wubiToCh
   forever $ do
-    putStr "> "
-    hFlush stdout
-    l <- getLine
+    l <- fromMaybe "" <$> getInputLine "> "
     let
       r = maybeToList (M.lookup l w2c) ++
         concat (maybeToList $ M.lookup l c2w)
-    putStrLn $ unlines $ sortBy (comparing length `mappend` compare) $ 
-      map (concatMap (showWK . qToWK)) r
+    io . putStrLn $ if l == "" then "" else
+      unlines . sortBy (comparing length `mappend` compare) $
+        map (concatMap (showWK . qToWK)) r
