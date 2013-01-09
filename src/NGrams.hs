844b80305c15997c431385058c73e2fd7a34d620
Daniel Corson <danl@alum.mit.edu>
2013-01-09 10:22:56 -0500

    book_hard_words (wanted to see rare words in Moby Dick during annual reading)

---
+++ src/NGrams.hs
@@ -0,0 +1,21 @@
+module NGrams where
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char as BSC
+
+data NGram = NGram
+    { ngFreq :: Double
+    , ngWord :: BS.ByteString
+    , ngPartOfSpeech :: BS.ByteString
+    }
+
+readNGrams :: FilePath -> IO [NGram]
+readNGrams f = map readNGramLine . BS.lines <$> BS.readFile f
+
+readNGramLine :: BS.ByteString -> NGram
+readNGramLine l = NGram (read $ BSC.unpack freqS) word partOfSpeech
+  where
+    (freqS, tabRest) = BSC.break (== '\t') l
+    rest = BS.tail tabRest
+    (wordUnderscore, partOfSpeech) = BSC.breakEnd (== '_') $ rest
+    word = BS.init wordUnderscore
---
+++ src/book_hard_words.hs
@@ -0,0 +1,25 @@
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char as BSC
+import qualified Data.Map as M
+
+getWords :: BS.ByteString -> [BS.ByteString]
+getWords =
+    filter (not . BS.null) .
+    -- map (BSC.map toLower . BSC.filter isAlpha) . 
+    concatMap BSC.words . BSC.lines .
+    BSC.map (\ c -> if isAlpha c then toLower c else ' ')
+
+main :: IO ()
+main = do
+    ws <- getWords <$> BS.readFile "mobydick.txt"
+
+    -- putStr . unlines . map show . reverse . sort . 
+    --     map (\ x -> (length x, head x)) . group . sort $ ws
+    
+    wdToFreq <- M.fromListWith (+) . map (\ ng -> (ngWord ng, ngFreq ng)) <$>
+        readNGrams "/home/danl/p/l/melang/data/ngrams/cur/eng/out/1980l"
+
+    let wNub = nub $ sort ws
+        wFreq = map (\ w -> (M.findWithDefault 0 w wdToFreq, w)) wNub
+     
+    putStr . unlines . map show . reverse $ sort wFreq
