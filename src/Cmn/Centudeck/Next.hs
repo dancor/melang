f0b8a1c4a6f123abfc6f6739d5ff08fea2305969
Daniel Corson <danl@alum.mit.edu>
2013-09-03 14:08:12 +0800

    eng-simple

---
+++ src/Cmn/Centudeck/Next.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Applicative
+import Control.DeepSeq
+import qualified Data.Set as Set
+import qualified Data.Text as DT
+import qualified Data.Text.IO as DTI
+
+import Cmn.Centudeck
+
+-- | Example: Centuline "的" "PRT" 1680518088
+data Dictline = Dictline
+    { dlWord         :: !DT.Text
+    , dlPartOfSpeech :: !DT.Text
+    , dlOccurs       :: !Int
+    , dlN            :: !Int
+    } deriving (Show)
+
+instance NFData Dictline where
+    rnf (Dictline a b c d) = rnf a `seq` rnf b `seq` rnf c `seq` rnf d
+
+readDictline :: Int -> DT.Text -> Dictline
+readDictline n s =
+    case DT.splitOn "\t" s of
+      [w, p, o] -> Dictline w p (read $ DT.unpack o) n
+      x -> error $ "readDictline: " ++ show x
+
+showDictline :: Dictline -> DT.Text
+showDictline (Dictline w p o n) =
+    DT.intercalate "\t" [w, p, DT.pack $ show o, DT.pack $ show n]
+
+main :: IO ()
+main = do
+    let deckF = "/home/danl/p/l/melang/data/cmn/deck.txt"
+    deck <- map readCentuline . DT.lines <$> DTI.readFile deckF
+    let deckWordSet = Set.fromList $ map clWord deck
+    let dictF = "/home/danl/p/l/melang/data/cmn/dict"
+    dictNext <-
+        take 40 .
+        filter (not . (`Set.member` deckWordSet) . dlWord) .
+        zipWith readDictline [1..] . DT.lines <$> DTI.readFile dictF
+    DTI.putStr . DT.unlines $ map showDictline dictNext
