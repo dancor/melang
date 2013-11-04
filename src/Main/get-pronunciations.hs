ba6f0717f7346d777611a6a0c5c73a652f7130cd
Daniel Corson <danl@alum.mit.edu>
2013-11-04 23:30:49 +0800

    cp

--- src/Main/get-pronunciations.hs
+++ src/Main/get-pronunciations.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Applicative
+import Control.Arrow
+import Control.DeepSeq
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import qualified Data.Text as DT
@@ -9,6 +11,7 @@ import qualified Data.Text.IO as DTI
 import Cmn.Cedict
 import Cmn.GoogBk
 import Cmn.KiloDeck
+import Util.DT
 
 main :: IO ()
 main = do
@@ -16,9 +19,15 @@ main = do
     deckPronMap <- Map.fromList .
         map (\x -> (kLWord x, Set.singleton $ kLPinyin x)) <$>
         loadKiloDecks kiloDeckDir
+    let _ = rnf deckPronMap
+    let pronF = "/home/danl/p/l/melang/data/cmn/pinyin"
+    prevPronMap <- Map.fromList . map (second Set.singleton) .
+        map (breakOnCh '\t') . DT.lines <$> DTI.readFile pronF
+    let _ = rnf prevPronMap
     dictPronMap <- Map.fromListWith Set.union .
         map (\x -> (cSimp x, Set.singleton $ cPron x)) <$> loadCedict
-    let pronMap = Map.union deckPronMap dictPronMap
+    let _ = rnf dictPronMap
+    let pronMap = Map.union deckPronMap $ Map.union prevPronMap dictPronMap
         doGoogWd x = DT.concat [gWd x, "\t", pronStr]
           where
             pronStr = maybe pronByChr showAllPoss $ Map.lookup (gWd x) pronMap
@@ -26,4 +35,4 @@ main = do
                 sequence
                 [Map.lookup (DT.pack [c]) pronMap | c <- DT.unpack $ gWd x]
             showAllPoss = DT.intercalate " \\ " . Set.toList
-    DTI.putStr . DT.unlines $ map doGoogWd goog
+    DTI.writeFile pronF . DT.unlines $ map doGoogWd goog
