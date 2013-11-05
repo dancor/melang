39d52282448be0087df64afd1f7f5c064e079ec6
Daniel Corson <danl@alum.mit.edu>
2013-11-05 12:32:07 +0800

    revert pronByChr changes that did not work well

--- src/Main/get-pronunciations.hs
+++ src/Main/get-pronunciations.hs
@@ -38,8 +38,8 @@ main = do
     let _ = rnf deckPronMap
     let pronF = "/home/danl/p/l/melang/data/cmn/pinyin"
     prevPronMap <- Map.fromList . map (second Set.singleton) .
-        map (breakOnCh '\t') . filter (not . ("\\" `DT.isInfixOf`)) .
-        DT.lines <$> DTI.readFile pronF
+        map (breakOnCh '\t') .
+        filter (not . ("\\" `DT.isInfixOf`)) . DT.lines <$> DTI.readFile pronF
     let _ = rnf prevPronMap
     dictPronMap <- Map.fromListWith Set.union .
         map (\x -> (cSimp x, Set.singleton $ cPron x)) <$> loadCedict
@@ -49,10 +49,8 @@ main = do
         doGoogWd x = DT.concat [gWd x, "\t", pronStr]
           where
             pronStr = maybe pronByChr showAllPoss $ Map.lookup (gWd x) pronMap
-            pronByChr = maybe "?" handleEachChrPron eachChrPronMby
-            handleEachChrPron = DT.intercalate " | " .
-                map DT.concat . sequence . map Set.toList
-            eachChrPronMby = sequence
+            pronByChr = maybe "?" (DT.intercalate " | " . map showAllPoss) $
+                sequence
                 [Map.lookup (DT.pack [c]) pronMap | c <- DT.unpack $ gWd x]
             showAllPoss = DT.intercalate " \\ " . Set.toList
     DTI.writeFile pronF . DT.unlines $ map doGoogWd goog
