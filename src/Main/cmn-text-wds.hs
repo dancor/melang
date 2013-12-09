e17ff91d7293b827a315e321da3a180ca99dde00
Daniel Corson <danl@alum.mit.edu>
2013-12-10 00:23:34 +0800

    cmn-text-wds treat files

--- src/Main/cmn-text-wds.hs
+++ src/Main/cmn-text-wds.hs
@@ -1,11 +1,25 @@
 import Control.Applicative
+import Control.DeepSeq
+import Control.Monad
+import Data.List
+import qualified Data.Text as DT
 import qualified Data.Text.IO as DTI
+import System.Environment
 
 import Cmn.Dict
 import Cmn.TextWds
+
+dropIfEnds :: String -> String -> String
+dropIfEnds suff x
+  | suff `isSuffixOf` x = take (length x - length suff) x
+  | otherwise = x
 
 main :: IO ()
 main = do
     dict <- dictToWdDict <$> loadDict
     let maxWdLen = 7
-    DTI.interact (textWdsHtml dict maxWdLen)
+    args <- getArgs
+    forM_ args $ \arg -> do
+        c <- DTI.readFile arg
+        DTI.writeFile (dropIfEnds ".txt" arg ++ ".html") $!!
+            textWdsHtml dict maxWdLen $!! c
