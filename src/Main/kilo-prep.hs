b6d6229ee444c2ca15cf7f69773c26ea7d4d731d
Daniel Corson <danl@alum.mit.edu>
2013-12-14 23:35:26 +0800

    kilo-prep show undefined word characters

--- src/Main/kilo-prep.hs
+++ src/Main/kilo-prep.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.DeepSeq
+import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Writer
 import Data.Char
 import qualified Data.HashMap.Strict as HMS
 import Data.List
 import Data.List.Split
 import qualified Data.Map.Strict as Map
 import Data.Monoid
+import qualified Data.Set as Set
 import qualified Data.Text as DT
 import qualified Data.Text.IO as DTI
 import System.FilePath
 
 import Cmn.Dict
+
+type Misses = Set.Set DT.Text
 
 -- | Remove any prefix like "#1:", "#2:", ..
 killNum :: DT.Text -> DT.Text
@@ -28,28 +33,37 @@ kiloDeckDir = "/home/danl/p/l/melang/data/cmn/kilo-deck"
 intToDeckName :: Int -> FilePath
 intToDeckName n = "mando-gloss-" ++ show n ++ "k.txt"
 
-prepSubEntry :: WdDict -> DT.Text -> DT.Text
-prepSubEntry wdDict word = "<br>" <> word <> " " <>
+prepSubEntry :: WdDict -> DT.Text -> Writer Misses DT.Text
+prepSubEntry wdDict word = (("<br>" <> word <> " ") <>) <$>
     case HMS.lookup word wdDict of
-      Nothing -> "?"
-      Just entry -> DT.intercalate " " [ePinyin entry, eGloss entry,
-        DT.replace "ADP" "PREP" (eSpParts entry), eSpPartFreqs entry]
+      Nothing -> tell (Set.singleton word) >> return "?"
+      Just entry -> do
+        let gloss = eGloss entry
+        when (":" `DT.isSuffixOf` gloss) $ tell (Set.singleton word)
+        return $ DT.intercalate " " [ePinyin entry, gloss,
+            DT.replace "ADP" "PREP" (eSpParts entry), eSpPartFreqs entry]
 
-prepEntry :: WdDict -> DictEntry -> DT.Text
-prepEntry wdDict entry =
-    eWord entry <> "\t" <> ePinyin entry <> "\t" <> eGloss entry <> "\t" <>
-    DT.replace "ADP" "PREP" (eSpParts entry) <> " " <> eSpPartFreqs entry <>
-    if DT.length word == 1
-      then ""
-      else 
-        DT.concat . map (prepSubEntry wdDict . DT.singleton) $ DT.unpack word
-  where
-    word = eWord entry
+prepEntry :: WdDict -> DictEntry -> Writer Misses DT.Text
+prepEntry wdDict entry = do
+    let word = eWord entry
+    subEntries <- if DT.length word == 1
+      then return ""
+      else do
+        DT.concat <$> mapM (prepSubEntry wdDict . DT.singleton)
+            (DT.unpack word)
+    return $
+        eWord entry <> "\t" <> ePinyin entry <> "\t" <>
+        eGloss entry <> "\t" <>
+        DT.replace "ADP" "PREP" (eSpParts entry) <> " " <>
+        eSpPartFreqs entry <> subEntries
 
-writeKiloDeck :: FilePath -> WdDict -> Dict -> IO ()
-writeKiloDeck f wdDict = DTI.writeFile f . DT.unlines . map (prepEntry wdDict)
+writeKiloDeck :: FilePath -> WdDict -> Dict -> WriterT Misses IO ()
+writeKiloDeck f wdDict dict = do
+    let (res, misses) = runWriter $ mapM (prepEntry wdDict) dict
+    tell misses
+    lift . DTI.writeFile f $ DT.unlines res
 
-writeKiloDecks :: FilePath -> WdDict -> Dict -> IO ()
+writeKiloDecks :: FilePath -> WdDict -> Dict -> WriterT Misses IO ()
 writeKiloDecks dir wdDict = zipWithM_ (\n ->
     writeKiloDeck (dir </> intToDeckName n) wdDict) [1..] . chunksOf 1000
 
@@ -99,5 +113,6 @@ main = do
     let dict = deDupeGlosses . deDupePinyins $ map entryKillNums oldDict
         wdDict = dictToWdDict dict
     writeDict dict
-    writeKiloDecks kiloDeckDir wdDict $
+    ((), misses) <- runWriterT . writeKiloDecks kiloDeckDir wdDict $
         takeWhile (not . glossIsEmpty . eGloss) dict
+    DTI.putStr . DT.unlines $ Set.toList misses
