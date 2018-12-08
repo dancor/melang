fe69f9ab1bb7d6303531c0e3ce54713d2e76c632
Daniel P Corson <danl@alum.mit.edu>
2018-12-07 18:17:04 -0800

    cp

--- src/Lang/Zh/Cedict.hs
+++ src/Lang/Zh/Cedict.hs
@@ -33,33 +33,6 @@ data CedictEntry = CedictEntry
 
 type Cedict = HashMap Text CedictEntry
 
-{-
-
-
-type WidthMarkedStr = Vector (Char, Int)
-
--- Currently this calls isExon up to twice as much as it needs to?
-findLastExonIndices :: (a -> Bool) -> Vector a -> [Int]
-findLastExonIndices isExon v = go 0 where
-  go i = case V !? i of
-    Nothing -> []
-    Just val -> if isExon val
-      then case V !? (i + 1) of
-        Nothing -> [i]
-        Just val2 -> if isExon val2 then go (i + 1) else i : go (i + 1)
-      else go (i + 1)
-
--- Given a vector with length > n, truncate it to length <= n,
--- truncating at the end of an isExon section.
---
--- When there are no isExon regions, just truncate to length n.
-truncTryAfterExon :: Int -> (a -> Bool) -> Vector a -> Vector a
-truncTryAfterExon n isExon v =
-  case filter (< n) $ findLastExonIndexes isExon v of
-    [] -> V.take n v
-    res -> last res
--}
-
 dotDotAfterWord :: Int -> Text -> Text
 dotDotAfterWord n t =
     case go Nothing 0 0 of
@@ -85,76 +58,8 @@ dotDotAfterWord n t =
                 else lastGood
         in  go lastGood' widthSoFar' (i + 1)
 
-go :: Int -> Text -> Maybe Int -> Int -> Int -> Maybe Int
-go maxWidth t lastGood widthSoFar i = if i >= T.length t || widthSoFar > maxWidth
-  then lastGood
-  else
-    let c = t `T.index` i
-        width = wcwidth c
-        lastGood' =
-          if not (isSpace c) &&
-              (i + 1 >= T.length t || isSpace (t `T.index` (i + 1)))
-            then Just i
-            else lastGood
-    in  go maxWidth t lastGood' (widthSoFar + width) (i + 1)
-{-
-  where
-
-  if  | not (V.any isInter vTrunc) -> Just vTrunc
-      | isInter (v ! (n - 2)) || isInter (v ! (n - 3)) ->
-        T.dropWhileEnd isSpace tTrunc <> ".."
-      | otherwise ->
-        T.dropWhileEnd isSpace $ T.dropWhileEnd (not . isSpace) tTrunc <> ".."
-  where
-
-
-dotDotAfterWord :: Int -> Text -> Text
-dotDotAfterWord n t =
-  if
-    | tWidth <= n -> t
-    | not (T.any isSpace tTrunc) -> tTrunc <> ".."
-    | isSpace (t `T.index` (n - 2)) || isSpace (t `T.index` (n - 3)) ->
-      T.dropWhileEnd isSpace tTrunc <> ".."
-    | otherwise ->
-      T.dropWhileEnd isSpace $ T.dropWhileEnd (not . isSpace) tTrunc <> ".."
-  where
-    tWithWidths = map (\c -> (c, wcwidth c)) $ T.unpack t
-    cumulativeTWithWidths =
-        scanl (\(t1,w1) (t2,w2) -> (t1 ++ [t2], w1 + w2)) ("", 0) tWithWidths
-    tTrunc = T.pack . fst . lastNote "dotDotAfterWord tTrunc last failed" $
-        filter ((<= n - 2) . snd) cumulativeTWithWidths
-    tWidth = sum $ map snd tWithWidths
-
--- If the text t has length greater than n, truncate and append ".." with
--- result length n or less, truncating at a word boundary if possible.
--- n must be more than 2.
--- 
--- TODO: Account for double-width characters.
-dotDotAfterWord :: Int -> Text -> Text
-dotDotAfterWord n t =
-  if
-    | tWidth <= n -> t
-    | not (T.any isSpace tTrunc) -> tTrunc <> ".."
-    | isSpace (t `T.index` (n - 2)) || isSpace (t `T.index` (n - 3)) ->
-      T.dropWhileEnd isSpace tTrunc <> ".."
-    | otherwise ->
-      T.dropWhileEnd isSpace $ T.dropWhileEnd (not . isSpace) tTrunc <> ".."
-  where
-    tWithWidths = map (\c -> (c, wcwidth c)) $ T.unpack t
-    cumulativeTWithWidths =
-        scanl (\(t1,w1) (t2,w2) -> (t1 ++ [t2], w1 + w2)) ("", 0) tWithWidths
-    tTrunc = T.pack . fst . lastNote "dotDotAfterWord tTrunc last failed" $
-        filter ((<= n - 2) . snd) cumulativeTWithWidths
-    tWidth = sum $ map snd tWithWidths
--}
-
 condenseMandarin :: Text -> Text
 condenseMandarin = id
-
--- TODO: Have the gloss truncation length dependent on the number of Mandarin
--- characters.
-polishGloss :: Text -> Text
-polishGloss = dotDotAfterWord 40
 
 parseCedictEntry :: Text -> (Text, CedictEntry)
 parseCedictEntry l = (simp, CedictEntry pinyin gloss)
@@ -163,7 +68,7 @@ parseCedictEntry l = (simp, CedictEntry pinyin gloss)
     simp = T.takeWhile (/= ' ') $ T.tail $ T.dropWhile (/= ' ') tradSimpPinyin
     pinyin = T.init $ T.init $ T.tail $ T.dropWhile (/= '[') tradSimpPinyin
     defs = init defsAndEmpty
-    gloss = polishGloss $ minimumBy (compare `on` T.length) defs
+    gloss = dotDotAfterWord 40 $ minimumBy (compare `on` T.length) defs
 
 takeShorter :: CedictEntry -> CedictEntry -> CedictEntry
 takeShorter a@(CedictEntry _ aG) b@(CedictEntry _ bG) =
