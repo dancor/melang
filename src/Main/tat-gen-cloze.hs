f30c60efb9066b17764ebca84d9fe907c304b312
Daniel P Corson <danl@alum.mit.edu>
2021-10-11 20:51:55 +0100

    cp

--- src/Main/tat-gen-cloze.hs
+++ src/Main/tat-gen-cloze.hs
@@ -14,10 +14,12 @@ import qualified Data.Text.Lazy.IO as T
 import Data.Hashable (Hashable, hashWithSalt)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
-import Data.List (foldl', minimumBy, sortBy)
-import Data.Maybe (catMaybes, fromMaybe)
+import Data.List (foldl', minimumBy, sort)
+import Data.Maybe (catMaybes, fromMaybe, fromJust)
 import Data.Ord (comparing)
 import System.Directory (getHomeDirectory)
 import System.FilePath ((</>))
@@ -43,51 +45,39 @@ data Tat = Tat
   , tSent :: !T
   } deriving Show
 
+data Cloze = Cloze
+  { cPos   :: Int
+  , cSent  :: T
+  , cTrans :: T
+  } deriving (Eq, Ord, Show)
+
 untab :: T -> [T]
 untab = T.split (== '\t')
 
 procTat :: T -> Maybe (I, Tat)
-procTat l = let [id, lang, sent] = untab l in
-  if lang == "deu" || lang == "por"
+procTat l = let [id, lang, sent] = untab l in if lang == "deu" || lang == "por"
   then Just (read $ T.unpack id, Tat lang sent) else Nothing
 
 tInt :: T -> I
 tInt = read . T.unpack
 
---procSent :: HashMap T I -> (I, Tat) -> a
---procSent wordCounts (i, 
-
-{-
--- FIXME: This always returns Nothing?
--- Prefer to cloze words that are not capitalized in Portuguese.
--- This is a simple way to exclude proper nouns, such as names (eg. John),
--- which tend to be too easy to translate (eg. they may not even change).
--- This also means we try to avoid using the first word in the sentence too
--- (since it is usually, or always?, capitalized), but that's ok.
-genCloze :: IntMap Tat -> HashMap T I -> T -> Maybe (I,T,T)
-genCloze idToTat wordCounts line = let [id1, id2] = T.split (== '\t') line in
+lineToPorDeu :: IntMap Tat -> T -> Maybe (T,T)
+lineToPorDeu !idToTat line = let [id1, id2] = T.split (== '\t') line in
   case (IM.lookup (tInt id1) idToTat, IM.lookup (tInt id2) idToTat) of
-    (Just (Tat "por" s1), Just (Tat "deu" s2)) -> let
-      procWd wdNum wd1 wd2 = let
-        wd1Alpha = T.filter isAlpha wd1; wd1Clean = T.toLower wd1Alpha
-        wd2Alpha = T.filter isAlpha wd2; wd2Clean = T.toLower wd1Alpha
-        in case (wd1Clean, wd1Clean == wd2Clean, HM.lookup wd1Clean wordCounts) of
-        ("", _,     _)             -> trace ("1:" ++ show (s1, s2)) Nothing
-        ( _, _,     Nothing)       -> error $ 
-          "Word " ++ show wd1Clean ++ " not in ~/data/t/cur/word-count-por.tsv"
-        ( _, False, Just wd1Count) -> Just $ if wd1Alpha == wd1Clean
-          then Left  (wd1Count, wdNum)
-          else Right (wd1Count, wdNum)
-        --_ -> Nothing
-        _ -> trace ("2:" ++ show (s1, s2)) Nothing
-      in case partitionEithers $ catMaybes $
-      zipWith3 procWd [0..] (T.words s1) (cycle $ T.words s2) of
-      --([],[]) -> Nothing
-      ([],[]) -> trace ("3:" ++ show (s1, s2)) Nothing
-      ([], l) -> Just (snd $ minimum l, s1, s2)
-      ( l, _) -> Just (snd $ minimum l, s1, s2)
-    _ -> Nothing
--}
+  (Just (Tat "por" s1), Just (Tat "deu" s2)) -> Just (s1, s2); _ -> Nothing
+
+-- Consider por words where the clean form isn't "" and doesn't match the
+-- clean form of any deu word. Take one that is rarest by total word count
+-- from all the Portuguese sentences.
+porDeuBestI :: HashMap T I -> (T,T) -> I
+porDeuBestI !porWdCounts (!por,!deu) = let
+  contenders = sort . map (\(i,porCleanWd) ->
+    (fromJust $ HM.lookup porCleanWd porWdCounts, i)) .
+    filter (not . (`HS.member` deuCleanWds) . snd) .
+    filter (not . T.null . snd) $ zip [0..] (map clean $ T.words por)
+  deuCleanWds = HS.fromList . filter (not . T.null) . map clean $ T.words deu
+  clean = T.toLower . T.filter isAlpha
+  in case contenders of (_,i):_ -> i; _ -> error $ show (por, deu)
 
 readWordCounts :: IO (HashMap T I)
 readWordCounts = do
@@ -102,11 +92,25 @@ readSentences = do
   let curTatDir = home </> "data" </> "t" </> "cur"
   catMaybes . map procTat . T.lines <$> T.readFile (curTatDir </> "sentences.tsv")
 
+genMem :: Cloze -> (T,[T])
+genMem (Cloze p s t) =
+  ( t <> "\\n" <> T.replace "|" "\\|"
+    (T.replace "\\" "\\\\" . T.unwords $
+    take p wds ++ "______" : drop (p + 1) wds)
+  , [wds !! p]
+  ) where wds = T.words s
+
 main :: IO ()
 main = do
-  hPutStrLn stderr "Reading wordCounts."
-  wordCounts <- readWordCounts
   hPutStrLn stderr "Reading sentences."
   idToTat <- IM.fromList <$> readSentences
-  clozes <- catMaybes . map (genCloze idToTat wordCounts) . T.lines <$> T.getContents
-  mapM_ (\(n,s1,s2) -> T.putStrLn $ T.pack (show n) <> "\n" <> s1 <> "\n" <> s2) clozes
+  ls <- T.lines <$> T.getContents
+  let porDeus = catMaybes $ map (lineToPorDeu idToTat) ls
+  hPutStrLn stderr $ "There were " ++ show (length porDeus) ++ 
+    " sentence pairs from Portuguese to German."
+  hPutStrLn stderr "Reading wordCounts."
+  porWordCounts <- readWordCounts
+  let clozes = map (\(por,deu) ->
+        Cloze (porDeuBestI porWordCounts (por,deu)) por deu) porDeus
+  mapM_ (\(q,a) -> T.putStrLn $ q <> "|" <> T.intercalate "," a) $ HM.toList $
+    HM.fromListWith (++) $ map genMem clozes
